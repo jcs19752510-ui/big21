b9b14c0d1827bf5b92719e75b1fbf13bff2e958b
jcs1975 <jcs197510@gmail.com>
2026-05-29 12:01:30 +0900

    20260529 333

--- chap05/mod1.py
+++ chap05/mod1.py
@@ -1,3 +1,10 @@
 def sum(a,b):
     return a + b    
-    
+
+def safe_sum(a,b):
+    if type(a) != type(b):
+        print("더할 수 있는 값이 아닙니다.")
+        return
+    else:
+        return a + b    
+        
